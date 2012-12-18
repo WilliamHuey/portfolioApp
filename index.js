8e4d240c9d455e74d19790bae20810490b61c8bb
WilliamHuey <lliw1236@yahoo.com>
2012-12-17 20:53:02 -0800

    add jquery

--- index.js
+++ index.js
@@ -10,9 +10,13 @@ app.configure(function(){
    app.use(express.static(path.join(__dirname, 'public')));
 });
 
-//app.get('/', home.index);
-app.get('/', function(req,res){
+app.get('/',function(req,res){
    res.render('home');
+});
+
+//app.get('/', home.index);
+app.get('/:viewname', function(req,res){
+   res.render(req.params.viewname);
 });
 
 app.listen(3000);
