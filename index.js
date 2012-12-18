af6852d311190654ee2a50528749774513e6f1c9
WilliamHuey <lliw1236@yahoo.com>
2012-12-17 20:33:45 -0800

    jade setup

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var express = require('express'),
-    home = require('./routes/home.js');
+    //home = require('./routes/home.js'),
+    path = require('path');
 
 var app = express();
 
-app.get('/', home.index);
+app.configure(function(){
+   app.set('view engine', 'jade');
+   app.set('views', __dirname + '/views');
+   app.use(express.static(path.join(__dirname, 'public')));
+});
+
+//app.get('/', home.index);
+app.get('/', function(req,res){
+   res.render('home');
+});
 
 app.listen(3000);
